0a1d9e26f2df55f103b0dbd8683f5515da2743ea
Veronica Adler <vlynnadler@gmail.com>
2022-04-10 22:44:04 -0400

    netninja next.js tutorial up to lesson 5

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Cupcake ipsum dolor sit amet dessert carrot cake. Lollipop sugar plum fruitcake lollipop gingerbread gingerbread donut. Macaroon candy sweet oat cake soufflé I love chocolate bar gingerbread.</p>
+        </div>
+      );
+}
+ 
+export default About;
